b87ac151ba1049c2db17d19292669e4a745f914f
Todd Halfpenny <todd@toddhalfpenny.com>
2015-09-16 16:22:59 +0100

    Adding dist files for mocks

--- dist/ng-cordova-mocks.js
+++ dist/ng-cordova-mocks.js
@@ -1104,6 +1104,77 @@ ngCordovaMocks.factory('$cordovaDialogs', ['$q', function ($q) {
 
 /**
  * @ngdoc service
+ * @name ngCordovaMocks.cordovaFacebook
+ *
+ * @description
+ * A service for testing Facebook features
+ * in an app built with ngCordova.
+ **/
+ngCordovaMocks.factory('$cordovaFacebook', ['$q', function ($q) {
+  return {
+
+    /**
+     * These properties are here for the purpose of automated testing only.
+     **/
+    loginShouldSucceedWith: null,
+    showDialogShouldSucceedWith: null,
+    apiShouldSucceedWith: null,
+    getAccessTokenShouldSucceedWith: null,
+    getLoginStatusShouldSucceedWith: null,
+    logoutShouldSuceedWith: null,
+
+    login: function (permissions) {
+      if (this.loginShouldSucceedWith !== null) {
+        return $q.when(this.loginShouldSucceedWith);
+      } else {
+        return $q.reject();
+      }
+    },
+
+    showDialog: function (options) {
+      if (this.showDialogShouldSucceedWith !== null) {
+        return $q.when(this.showDialogShouldSucceedWith);
+      } else {
+        return $q.reject();
+      }
+    },
+
+    api: function (path, permissions) {
+      if (this.apiShouldSucceedWith !== null) {
+        return $q.when(this.apiShouldSucceedWith);
+      } else {
+        return $q.reject();
+      }
+    },
+
+    getAccessToken: function () {
+      if (this.getAccessTokenShouldSucceedWith !== null) {
+        return $q.when(this.getAccessTokenShouldSucceedWith);
+      } else {
+        return $q.reject();
+      }
+    },
+
+    getLoginStatus: function () {
+      if (this.getLoginStatusShouldSucceedWith !== null) {
+        return $q.when(this.getLoginStatusShouldSucceedWith);
+      } else {
+        return $q.reject();
+      }
+    },
+
+    logout: function () {
+      if (this.logoutShouldSuceedWith !== null) {
+        return $q.when(this.logoutShouldSuceedWith);
+      } else {
+        return $q.reject();
+      }
+    }
+  };
+}]);
+
+/**
+ * @ngdoc service
  * @name ngCordovaMocks.cordovaFile
  *
  * @description
@@ -2223,6 +2294,129 @@ ngCordovaMocks.factory('$cordovaKeychain', ['$q', function ($q) {
   };
 }]);
 
+
+/**
+ * @ngdoc service
+ * @name ngCordovaMocks.localNotification
+ *
+ * @description
+ * A service for testing LocalNotificatio
+ * in an app build with ngCordovaMocks.
+ */
+
+ngCordovaMocks.factory('$cordovaLocalNotification', ['$q', function ($q) {
+
+  var storageKeyPfx  = "ngCordLocNotif-";
+  
+  function pfxId(id) {
+    return storageKeyPfx + id;
+  }
+
+  function getAllIds(){
+    var defer = $q.defer();
+    var locNotifIds = [];
+    for ( var i = 0, len = localStorage.length; i < len; ++i ) {
+      if (localStorage.key( i ).indexOf(storageKeyPfx) > -1)
+        locNotifIds.push(parseInt(localStorage.key( i ).split("-")[1]));
+    }
+    defer.resolve(locNotifIds);
+    return defer.promise;
+  }
+
+
+  return {
+    cancel: function (ids) {
+      var defer = $q.defer();
+      if (typeof(ids) == "number") ids = [ids];
+      ids.forEach(function(id){
+        localStorage.removeItem([pfxId(id)]);
+      });      
+      defer.resolve();
+      return defer.promise;
+    },
+    cancelAll: function () {
+      var defer = $q.defer();
+      // TODO
+      defer.resolve();
+      return defer.promise;
+    },
+    clear: function (ids) {
+      if (typeof(ids) == "number") ids = [ids];
+      var defer = $q.defer();
+      ids.forEach(function(id){
+        localStorage.removeItem([pfxId(id)]);
+      });      
+      defer.resolve();
+      return defer.promise;
+    },
+    clearAll: function () {
+      var defer = $q.defer();
+      // TODO
+      // defer.resolve();
+      return defer.promise;
+    },
+    isScheduled: function (id){
+      var defer = $q.defer();
+      if (localStorage[pfxId(id)]) {
+        defer.resolve(true);
+      } else {
+        defer.resolve(false);
+      }
+      return defer.promise;
+    },
+    isPresent: function (id){
+      var defer = $q.defer();
+      if (localStorage[pfxId(id)]) {
+        defer.resolve(true);
+      } else {
+        defer.resolve(false);
+      }
+      return defer.promise;
+    },
+    isTriggered: function (id){
+      var defer = $q.defer();
+      if (localStorage[pfxId(id)]) {
+        defer.resolve(false);
+      } else {
+        defer.resolve(true);
+      }
+      return defer.promise;
+    },
+    getAllIds: function() {
+      return getAllIds();
+    },
+    getIds: function() {
+      return getAllIds();
+    },
+    getScheduledIds:  function() {
+      return getAllIds();
+    },
+    getTriggeredIds: function() {
+      var defer = $q.defer();
+      defer.resolve([]);
+      return defer.promise;
+    },
+    hasPermission: function (id){
+      var defer = $q.defer();
+      defer.resolve(true);
+      return defer.promise;
+    },
+    schedule: function (data){
+      var defer = $q.defer();
+      var id = pfxId(data.id);
+      localStorage[id] = JSON.stringify(data);
+      defer.resolve();
+      return defer.promise;
+    },
+    update: function (data){
+      var defer = $q.defer();
+      var id = pfxId(data.id);
+      localStorage[id] = JSON.stringify(data);
+      defer.resolve();
+      return defer.promise;
+    }
+  };
+}]);
 /**
  * @ngdoc service
  * @name ngCordovaMocks.cordovaNetwork
